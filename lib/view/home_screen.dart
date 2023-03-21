9d00c65e817191cea94b8e1f6bf3eb3fac58725b
habeebu <habeebrh321@gmail.com>
2023-03-21 06:55:51 +0530

    implemented a floating action  button

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -8,39 +8,47 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<HomeController>(context, listen: false);
-    provider.showData();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Screen'),
       ),
-      body: SafeArea(child: Center(
-        child: Consumer<HomeController>(
-          builder: (context, value, child) {
-            return provider.isLoading == true
-                ? const Center(child: CircularProgressIndicator())
-                : ListView.separated(
-                    itemBuilder: (BuildContext context, int index) {
-                      final data = value.homeValue;
-                      return Column(
-                        children: [
-                          Text(data?.name ?? 'null'),
-                          Text(data?.climate ?? 'null'),
-                          Text(data?.diameter ?? 'null'),
-                          Text(data?.gravity ?? 'null'),
-                          Text(data?.population ?? 'null'),
-                          Text(data?.terrain ?? 'null'),
-                        ],
-                      );
-                    },
-                    itemCount: 10,
-                    separatorBuilder: (context, index) => const SizedBox(
-                      height: 20,
-                      child: Divider(thickness: 3),
-                    ),
-                  );
-          },
+      body: SafeArea(
+        child: Center(
+          child: Consumer<HomeController>(
+            builder: (context, value, child) {
+              return provider.isLoading == true
+                  ? const Center(child: CircularProgressIndicator())
+                  : ListView.separated(
+                      itemBuilder: (BuildContext context, int index) {
+                        final data = value.homeValue;
+                        return Column(
+                          children: [
+                            Text(data?.name ?? 'null'),
+                            Text(data?.climate ?? 'null'),
+                            Text(data?.diameter ?? 'null'),
+                            Text(data?.gravity ?? 'null'),
+                            Text(data?.population ?? 'null'),
+                            Text(data?.terrain ?? 'null'),
+                          ],
+                        );
+                      },
+                      itemCount: 10,
+                      separatorBuilder: (context, index) => const SizedBox(
+                        height: 20,
+                        child: Divider(thickness: 3),
+                      ),
+                    );
+            },
+          ),
         ),
-      )),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          provider.showData();
+        },
+        label: const Text('Fetch Api'),
+      ),
     );
   }
 }
